614dfa795231fbd64b2a29b9ca9b68b0e4bbdad1
Jeanne Pindar <jpindar@jpindar.com>
2021-10-11 16:11:38 -0400

    added most types

--- Hue1.py
+++ Hue1.py
@@ -19,11 +19,14 @@ some error types
 import logging
 import json
 import requests
+from typing import List, Dict, Optional, Any, Union
+Response = Dict[str, Any]
 
 ___author___ = "jpindar@jpindar.com"
 
 # If a light is physically on and off, you can turn it virtually on and off. But you can't set it's hue etc.
 LIGHT_IS_TURNED_OFF = 201
+
 ENABLE_LOGGING = True
 
 logger = logging.getLogger(__name__)
@@ -42,28 +45,28 @@ class HueError(Exception):
          description -- explanation of the error
     """
 
-    def __init__(self, type, description):
+    def __init__(self, type: int, description: str):
         self.type = type
         self.description = description
 
 
-def check_response_for_error(result):
+def check_response_for_error(result: List[Response]) -> None:
     for s in result:
         if 'error' in s:
-            type = s['error']['type']
-            description = (s['error']['description'])
+            type: int = s['error']['type']
+            description: str = (s['error']['description'])
             raise HueError(type, "error: " + description)
 
 
-def request(method, url, route, **kwargs):
+def request(method: str, url: str, route: str, **kwargs: Any) -> List[Response]:
     try:
-        response = requests.request(method, url + '/' + route, **kwargs)
+        response: requests.models.Response = requests.request(method, url + '/' + route, **kwargs)
         if response.status_code != requests.codes.ok:   # should be 200
             raise HueError(0, "Got bad response status from Hue Bridge")
         # response.json() can be either a dict or a list containing a dict
         # AFAIK, the list only ever has one element, but we can't be sure of that
         # so instead of stripping the dict out of the list, let's do the opposite
-        r = response.json()
+        r: Union[Response, List[Response]] = response.json()
         if isinstance(r, dict):
             r = [r]
         return r
@@ -77,24 +80,24 @@ def request(method, url, route, **kwargs):
         logger.error(e.args)
         raise e
     except Exception as e:
-        logger.error("unknown error condition in request() method")
+        logger.error("unknown error condition")
         logger.error(e.args)
         raise e
 
 
 class Bridge:
 
-    def __init__(self, ip_address, username):
-        self.light_list = []
-        self.scene_list = []
-        self.group_list = []
-        self.url = "http://" + ip_address + "/api/" + username
-        self.data = {}
+    def __init__(self, ip_address: str, username: str) -> None:
+        self.light_list: List[Light] = []
+        self.scene_list: List[Scene] = []
+        self.group_list: List[Group] = []
+        self.url: str = "http://" + ip_address + "/api/" + username
+        self.data: Dict[str, Any] = {}
 
-    def get_all_data(self):
+    def get_all_data(self) -> Dict[str, Any]:
         """ Get all data from the bridge. """
         try:
-            response = request("GET", self.url, '')
+            response: List[Response] = request("GET", self.url, '')
             check_response_for_error(response)
             self.data = response[0]
         except HueError as e:
@@ -102,37 +105,37 @@ class Bridge:
             raise e
         return self.data
 
-    def get_config(self):
+    def get_config(self) -> Dict[str, Any]:
         try:
-            response = request("GET", self.url, "config")
+            response: List[Response] = request("GET", self.url, "config")
             check_response_for_error(response)
         except HueError as e:
             logger.error(e.args)
             raise e
         return response[0]
 
-    def get_whitelist(self):
+    def get_whitelist(self) -> Dict[str, Any]:
         """ Get the bridge's whitelist of usernames """
-        config = self.get_config()
-        whitelist = config['whitelist']
+        config: Dict[str, Any] = self.get_config()
+        whitelist: Dict[str, Any] = config['whitelist']
         return whitelist
 
-    def delete_user(self, id):
+    def delete_user(self, id: str) -> None:
         route = "config/whitelist/" + str(id)
         try:
-            response = request("DELETE", self.url, route)
+            response: List[Response] = request("DELETE", self.url, route)
             check_response_for_error(response)
         except HueError as e:
             logger.error(e.args)
             raise e
 
-    def get_lights(self):
+    def get_lights(self) -> List['Light']:
         """
             Get a list of the bridge's lights.
             Note that light.index starts at 1 but list positions start at 0
         """
         try:
-            response = request("GET", self.url, Light.ROUTE)
+            response: List[Response] = request("GET", self.url, Light.ROUTE)
             check_response_for_error(response)
         except HueError as e:
             logger.error("Hue Error " + str(e.args))
@@ -142,9 +145,9 @@ class Bridge:
         self.light_list = sorted(self.light_list, key=lambda x: x.index)
         return self.light_list
 
-    def get_scenes(self):
+    def get_scenes(self) -> List['Scene']:
         try:
-            response = request("GET", self.url, Scene.ROUTE)
+            response: List[Response] = request("GET", self.url, Scene.ROUTE)
             check_response_for_error(response)
         except HueError as e:
             logger.error(e.args)
@@ -154,9 +157,9 @@ class Bridge:
         self.scene_list = sorted(self.scene_list, key=lambda x: x.name)
         return self.scene_list
 
-    def get_groups(self):
+    def get_groups(self) -> List['Group']:
         try:
-            response = request("GET", self.url, Group.ROUTE)
+            response: List[Response] = request("GET", self.url, Group.ROUTE)
             check_response_for_error(response)
         except HueError as e:
             logger.error(e.args)
@@ -167,60 +170,61 @@ class Bridge:
         self.group_list = sorted(self.group_list, key=lambda x: x.name)
         return self.group_list
 
-    def get_scene_by_name(self, desired_name):
+    def get_scene_by_name(self, desired_name: str) -> Optional['Scene']:
         self.get_scenes()
         for scene in self.scene_list:
             if scene.name == desired_name:
                 return scene
         return None
 
-    def get_scene_by_id(self, desired_id):
+    def get_scene_by_id(self, desired_id: str) -> Optional['Scene']:
         self.get_scenes()
         for scene in self.scene_list:
             if scene.id == desired_id:
                 return scene
         return None
 
-    def delete_scene(self, scene):
+    def delete_scene(self, scene: 'Scene') -> None:
         route = Scene.ROUTE + "/" + str(scene.id)
         try:
-            response = request("DELETE", self.url, route)
+            response: List[Response] = request("DELETE", self.url, route)
             check_response_for_error(response)
         except HueError as e:
             logger.error(e.args)
             raise e
 
-    def delete_group(self, group):
+    def delete_group(self, group: 'Group') -> None:
         route = Group.ROUTE + "/" + str(group.id)
         try:
-            response = request("DELETE", self.url, route)
+            response: List[Response] = request("DELETE", self.url, route)
             check_response_for_error(response)
         except HueError as e:
             logger.error(e.args)
             raise e
 
-    def lights(self):
+    def lights(self) -> List['Light']:
         # if we were going for speed at the expense of possible
         # errors (like if a light was added or removed from the bridge) we could add this:
         # note that physically turning a light off does not remove it from the bridge's list
-        if self.light_list == {}:
+        if self.light_list == []:
             self.get_lights()
         return self.light_list
 
-    def get_light_by_name(self, this_name):
+    def get_light_by_name(self, this_name: str) -> Optional['Light']:
         self.light_list = self.get_lights()
         for light in self.light_list:
             if light.name == this_name:
                 return light
         return None
 
-    def all_on(self, on):
-        group = Group(self, 0)  # group 0 is all lights
+    def all_on(self, on: bool) -> None:
+        group: Group = Group(self, 0)  # group 0 is all lights
         group.set('on', on)
 
-    def set_all(self, attr, value):
+    def set_all(self, attr: str, value: Any) -> None:
         """ Set all lights
             Since you can use group 0 for all lights, this is just an example.
+            value can be bool or int, not sure about str
         """
         self.get_lights()
         for light in self.light_list:
@@ -230,12 +234,12 @@ class Bridge:
 class Scene:
     ROUTE = 'scenes'
 
-    def __init__(self, bridge, id, data=None):
-        self.id = id
-        self.bridge = bridge
-        self.data = {}
-        self.name = ""
-        self.lights = []
+    def __init__(self, bridge: Bridge, id: str, data: Optional[Dict[str, Any]] = None) -> None:
+        self.id: str = id
+        self.bridge: Bridge = bridge
+        self.data: Dict[str, Any] = {}
+        self.name: str = ""
+        self.lights: List[str] = []
         if data is not None:
             try:
                 self.data = data
@@ -244,8 +248,8 @@ class Scene:
             except KeyError as e:
                 raise HueError(0, "Not able to parse scene data")
 
-    def display(self):
-        group = Group(self.bridge, 0)  # group 0 is all lights
+    def display(self) -> None:
+        group: Group = Group(self.bridge, 0)  # group 0 is all lights
         group.set("scene", self.id)
 
 
@@ -256,12 +260,12 @@ class Group:
 
     ROUTE = 'groups'
 
-    def __init__(self, bridge, id, data=None):
-        self.id = id
-        self.bridge = bridge
-        self.data = {}
-        self.name = ''
-        self.lights = []
+    def __init__(self, bridge: Bridge, id: int, data: Optional[Dict[str, Any]] = None) -> None:
+        self.id: int = id
+        self.bridge: Bridge = bridge
+        self.data: Dict[str, Any] = {}
+        self.name: str = ""
+        self.lights: List[str] = []
         if data is not None:
             try:
                 self.data = data
@@ -270,11 +274,11 @@ class Group:
             except KeyError as e:
                 raise HueError(0, "Not able to parse group data")
 
-    def set(self, attr, value):
+    def set(self, attr: str, value: Any) -> None:
         route = self.ROUTE + "/" + str(self.id) + "/action"
-        msg = json.dumps({attr: value})
+        msg: str = json.dumps({attr: value})
         try:
-            response = request("PUT", self.bridge.url, route, data=msg)
+            response: List[Response] = request("PUT", self.bridge.url, route, data=msg)
             #  r should be a list of dicts such as [{'success':{/lights/1/state/on':True}]
             #  1st element of 1st element == 'success'
             check_response_for_error(response)
@@ -286,12 +290,12 @@ class Group:
 class Light:
     ROUTE = 'lights'
 
-    def __init__(self, bridge, index, data=None):
-        self.index = int(index)
-        self.bridge = bridge
-        self.data = {}
-        self.name = ""
-        self.state = {}
+    def __init__(self, bridge: Bridge, index: int, data: Optional[Dict[str, Any]] = None) -> None:
+        self.index: int = int(index)
+        self.bridge: Bridge = bridge
+        self.data: Dict[str, Any] = {}
+        self.name: str = ""
+        self.state: Dict[str, Any] = {}
         if data is not None:
             try:
                 self.data = data
@@ -300,10 +304,10 @@ class Light:
             except KeyError as e:
                 raise HueError(0, "Not able to parse light data")
 
-    def get_data(self):
+    def get_data(self) -> Response:
         route = self.ROUTE + "/" + str(self.index)
         try:
-            response = request("GET", self.bridge.url, route)
+            response: List[Response] = request("GET", self.bridge.url, route)
             check_response_for_error(response)
             self.data = response[0]
             self.name = self.data['name']
@@ -313,7 +317,7 @@ class Light:
             logger.error(e.args)
             raise e
 
-    def set(self, attr, value):
+    def set(self, attr: str, value: Any) -> None:
         if isinstance(value, str):
             if value.lower() == "true":
                 value = True
@@ -323,10 +327,10 @@ class Light:
                 value = int(value)
         self.send(json.dumps({attr: value}))
 
-    def send(self, msg):
+    def send(self, msg: str) -> None:
         route = self.ROUTE + "/" + str(self.index) + "/state"
         try:
-            response = request("PUT", self.bridge.url, route, data=msg)
+            response: List[Response] = request("PUT", self.bridge.url, route, data=msg)
             #  r is a list of dicts such as [{'success':{/lights/1/state/on':True}]
             #  1st element of 1st element should be 'success'
             # it will be 'success' if the light is physically turned off
